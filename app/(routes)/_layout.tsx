fc610fc2a5ddc1784e4593d813f480551bbf7e58
Bnktop <bnktop@Bnktops-MacBook-Pro.local>
2025-03-24 23:32:07 +0100

    Initial router

---
+++ app/(routes)/_layout.tsx
@@ -0,0 +1,30 @@
+import { Stack } from "expo-router";
+import React from "react";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        header: () => null,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+        }}
+      />
+    </Stack>
+  );
+}
